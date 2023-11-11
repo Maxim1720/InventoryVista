8b4e1998b46c1cd1a42674430bf1aa715256f211
maksim1708 <musiev_maksim@mail.ru>
2023-11-11 13:19:32 +0500

    to rm auth state

--- InventoryVista-frontend/src/components/PageWithHeaderAndFooter.js
+++ InventoryVista-frontend/src/components/PageWithHeaderAndFooter.js
@@ -7,22 +7,17 @@ import 'cssstyle';
 class PageWithHeaderAndFooter extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {
-            isAuthorized: false
-        }
     }
 
 
     render() {
         return (
             <div className="d-flex flex-column h-100">
-                <Header isAuthorized={this.state.isAuthorized} />
+                <Header/>
                 <div className="flex-grow-1">
                         {this.props.content}
                 </div>
-                {/*<div className="flex-shrink-1">*/}
                     <Footer />
-                {/*</div>*/}
             </div>
         );
     }
